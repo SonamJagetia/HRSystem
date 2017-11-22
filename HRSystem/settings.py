1800d61a2500efa4cc00077352f143e577600050
SonamJagetia <sonamvjagetia@gmail.com>
2017-11-22 12:10:46 +0530

    Final commit

--- HRSystem/settings.py
+++ HRSystem/settings.py
@@ -79,6 +79,17 @@ WSGI_APPLICATION = 'HRSystem.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
+    }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -86,7 +97,7 @@ DATABASES = {
         'USER': 'dbtest',
         'PASSWORD': 'dbtest',
         'HOST': '127.0.0.1',
-        'PORT': '',
+        'PORT': '3306',
     }
 }
 
